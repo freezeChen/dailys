488041c104315e4d229c575dd14331a1d2e301c8
frozen <wancsc@163.com>
2018-05-07 23:09:06 +0800

    1

--- app/src/main/java/com/frozen/dailys/components/service/ImService.kt
+++ app/src/main/java/com/frozen/dailys/components/service/ImService.kt
@@ -1,19 +1,93 @@
 package com.frozen.dailys.components.service
 
-import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.frozen.dailys.components.util.RxManage
+import com.orhanobut.logger.Logger
+import io.reactivex.disposables.CompositeDisposable
+import okhttp3.*
+import okio.ByteString
 
 
-class ImService : Service() {
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class ImService : Service(), RxManage {
+    override var mCompositeDisposable: CompositeDisposable? = null
+
+    private var reConnTimes = 0
+
+    private var mWebSocket: WebSocket? = null
+
+    private lateinit var mListener: WebSocketListener
+
+    private val mClient by lazy {
+        OkHttpClient()
+    }
+
+    private val request by lazy {
+        Request.Builder()
+                .url("ws://47.106.137.3:8088/daily/socket")
+                .build()
+    }
+
+
     override fun onBind(intent: Intent?): IBinder? {
-
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+
+        mListener = object : WebSocketListener() {
+
+            override fun onOpen(webSocket: WebSocket?, response: Response?) {
+                super.onOpen(webSocket, response)
+                Logger.e("onOpen", response)
+                mWebSocket = webSocket
+                reConnTimes = 0
+            }
+
+            override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                Logger.e("onFailure", t)
+                reConnTimes++
+                if (reConnTimes <= 5) {
+                    mClient.newWebSocket(request, mListener)
+                }
+            }
+
+            override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
+                super.onClosing(webSocket, code, reason)
+                Logger.e("onFailure", reason)
+            }
+
+            override fun onMessage(webSocket: WebSocket?, text: String?) {
+                super.onMessage(webSocket, text)
+                Logger.e("onFailure", text)
+            }
+
+            override fun onMessage(webSocket: WebSocket?, bytes: ByteString?) {
+                super.onMessage(webSocket, bytes)
+                Logger.e("onFailure", bytes.toString())
+            }
+
+            override fun onClosed(webSocket: WebSocket?, code: Int, reason: String?) {
+                super.onClosed(webSocket, code, reason)
+                Logger.e("onFailure", reason)
+            }
+        }
+
+
+        mClient.newWebSocket(request, mListener)
+        mClient.dispatcher().executorService().shutdown()
         return super.onStartCommand(intent, flags, startId)
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unDisposable()
     }
 
 }
---
+++ app/src/main/java/com/frozen/dailys/components/util/RxManage.kt
@@ -0,0 +1,24 @@
+package com.frozen.dailys.components.util
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+interface RxManage {
+    var mCompositeDisposable: CompositeDisposable?
+
+    fun addDisposable(disposable: Disposable) {
+        if (mCompositeDisposable == null || mCompositeDisposable!!.isDisposed) {
+            mCompositeDisposable = CompositeDisposable()
+        }
+        mCompositeDisposable!!.add(disposable)
+    }
+
+    fun unDisposable() {
+        if (mCompositeDisposable != null && !mCompositeDisposable!!.isDisposed) {
+            mCompositeDisposable!!.dispose()
+        }
+
+    }
+
+
+}
